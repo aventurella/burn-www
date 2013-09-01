67c3c4a5714848359e8bdda603add76136702da8
Adam Venturella <aventurella@gmail.com>
2013-09-01 14:46:17 -0700

    more setup

--- burnlib/burnlib/tests/ioc.py
+++ burnlib/burnlib/tests/ioc.py
@@ -1,29 +1,16 @@
 from pydi import Container
-from pydi.utils import Resolve
-from bigmethod.core.configuration import settings
-from bigmethod.sqlalchemy.utils import sqlalchemy_session_factory
-from bigmethod.users.services import UserService
-from bigmethod.channels.services import ChannelService
-from bigmethod.clients.services import ClientService
-from bigmethod.projects.services import ProjectService
-from bigmethod.collectors.services import CollectorService
-from bigmethod.accounts.services import ChannelAccountService
-from bigmethod.jobs.services import JobService
-from bigmethod.sqlalchemy.resources.users import SqlAlchemyUserResource
-from bigmethod.sqlalchemy.resources.channels import SqlAlchemyChannelResource
-from bigmethod.sqlalchemy.resources.clients import SqlAlchemyClientResource
-from bigmethod.sqlalchemy.resources.projects import SqlAlchemyProjectResource
-from bigmethod.sqlalchemy.resources.collectors import SqlAlchemyCollectorResource
-from bigmethod.sqlalchemy.resources.accounts import SqlAlchemyChannelAccountResource
-from bigmethod.sqlalchemy.resources.jobs import SqlAlchemyJobResource
-
+from burnlib.core.configuration import settings
+from burnlib.sqlalchemy.utils import sqlalchemy_session_factory
 from burnlib.users.services import GameCenterService
 from burnlib.users.services import UserService
+
+from burnlib.sqlalchemy.drivers.users import UserDriver
+from burnlib.sqlalchemy.drivers.users import GameCenterDriver
 
 
 def sqlalchemy_session():
     session_factory = sqlalchemy_session_factory(
-        settings.data['bigmethod:sqlalchemy:test'],
+        settings.data['burn:sqlalchemy:test'],
         echo=False)
 
     return session_factory
@@ -33,25 +20,31 @@ def build_container(session):
 
     container = Container()
 
+    container.register(GameCenterService) \
+    .depends(GameCenterDriver, session_factory=session)
+
     container.register(UserService) \
-    .depends(SqlAlchemyUserResource, session_factory=session)
+    .depends(UserDriver, session_factory=session)
 
-    container.register(ChannelService) \
-    .depends(SqlAlchemyChannelResource, session_factory=session)
+    # container.register(UserService) \
+    # .depends(SqlAlchemyUserResource, session_factory=session)
 
-    container.register(ClientService) \
-    .depends(SqlAlchemyClientResource, session_factory=session)
+    # container.register(ChannelService) \
+    # .depends(SqlAlchemyChannelResource, session_factory=session)
 
-    container.register(ProjectService) \
-    .depends(SqlAlchemyProjectResource, session_factory=session)
+    # container.register(ClientService) \
+    # .depends(SqlAlchemyClientResource, session_factory=session)
 
-    container.register(CollectorService) \
-    .depends(SqlAlchemyCollectorResource, session_factory=session)
+    # container.register(ProjectService) \
+    # .depends(SqlAlchemyProjectResource, session_factory=session)
 
-    container.register(ChannelAccountService) \
-    .depends(SqlAlchemyChannelAccountResource, session_factory=session)
+    # container.register(CollectorService) \
+    # .depends(SqlAlchemyCollectorResource, session_factory=session)
 
-    container.register(JobService) \
-    .depends(SqlAlchemyJobResource, session_factory=session)
+    # container.register(ChannelAccountService) \
+    # .depends(SqlAlchemyChannelAccountResource, session_factory=session)
+
+    # container.register(JobService) \
+    # .depends(SqlAlchemyJobResource, session_factory=session)
 
     return container
